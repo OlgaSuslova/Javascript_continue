4c8fe6bbfb2b454b0bde13e65901461789412102
OlgaSuslova <alekssportmen@gmail.com>
2024-02-14 11:27:04 +0300

    added data for 6 lesson

---
+++ 6_homework/data.js
@@ -0,0 +1,52 @@
+const dataProducts = `
+[
+    {
+        "id": 1,
+        "image": "./img/product_1.svg",
+        "name": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring,Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": 52,
+        "currency": "usd"
+    },
+    {
+        "id": 2,
+        "image": "./img/product_2.svg",
+        "name": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring,Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": 52,
+        "currency": "usd"
+    },
+    {
+        "id": 3,
+        "image": "./img/product_3.svg",
+        "name": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring,Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": 52,
+        "currency": "usd"
+    },
+    {
+        "id": 4,
+        "image": "./img/product_4.svg",
+        "name": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring,Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": 52,
+        "currency": "usd"
+    },
+    {
+        "id": 5,
+        "image": "./img/product_5.svg",
+        "name": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring,Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": 52,
+        "currency": "usd"
+    },
+    {
+        "id": 6,
+        "image": "./img/product_6.svg",
+        "name": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring,Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": 52,
+        "currency": "usd"
+    }
+]
+`
---
+++ 6_sem/task1/data.js
@@ -0,0 +1,33 @@
+// Дан макет https://www.figma.com/file/mZwLT9fKktsWuVyfQRoST1/shop-(Copy)-(Copy)?node-id=73%3A140 в котором представлены товары на странице корзины
+// Необходимо создать файл data.js в котором создать переменную dataProducts в которую присвоить JSON данные по товарам.
+// Вам нужно самостоятельно создать JSON данные (не забыть про кавычки
+// Добавить все данные из макета, чтобы в дальнейшем по ним мы смогли создать вёрстку
+
+
+
+dataProducts = `
+[
+    {
+        "id": 1,
+        "image": "./img/img_1.jpg",
+        "name": "T-SHIRT",
+        "brand": "MANGO PEOPLE",
+        "price": 300,
+        "currency": "usd",
+        "color": "Red",
+        "size": "XL",
+        "quantity": 1
+    },
+    {
+        "id": 2,
+        "image": "./img/img_2.jpg",
+        "name": "CAPSULE",
+        "brand": "ELLERY X M'O",
+        "price": 52,
+        "currency": "usd",
+        "color": "black",
+        "size": "XL",
+        "quantity": 2
+    }
+]
+`
